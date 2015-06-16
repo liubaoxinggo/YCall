bb2fa2679b0f7ff56ca6237aed81f9748faa140a
liubaoxinggo <liubaoxinggo@foxmail.com>
2015-06-16 20:00:07 +0800

    2015-6-16主题布局的搭建初步完成【2】

---
+++ src/com/fhit/ycall/fragment/FavFragment.java
@@ -0,0 +1,33 @@
+package com.fhit.ycall.fragment;
+
+import com.fhit.ycall.R;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+@SuppressLint("ResourceAsColor") public class FavFragment extends Fragment {
+
+	Context mContext;
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		mContext = this.getActivity();
+	}
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		TextView view = new TextView(mContext);
+		view.setTextSize(108);
+		view.setTextColor(R.color.red);
+		view.setText(mContext.getResources().getString(R.string.fav));
+		return view;
+	}
+}
